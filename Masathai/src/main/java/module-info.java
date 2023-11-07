41bd7559c1418603a941c813003c9905747ac24e
GuyVanced <pandeykushal017@gmail.com>
2023-11-07 07:58:53 +0545

    Pushed the project folder into the repository

---
+++ Masathai/src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.example.masathai {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+            requires com.dlsc.formsfx;
+                    requires org.kordamp.bootstrapfx.core;
+            
+    opens com.example.masathai to javafx.fxml;
+    exports com.example.masathai;
+    exports com.example.masathai.Controllers;
+    opens com.example.masathai.Controllers to javafx.fxml;
+}
