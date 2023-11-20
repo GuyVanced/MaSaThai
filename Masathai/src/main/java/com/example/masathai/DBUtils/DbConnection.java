4c0c05173e2581ff02b0acac6df1b5f4f43b52aa
GuyVanced <pandeykushal017@gmail.com>
2023-11-20 23:38:51 +0545

    Database Connection Established

---
+++ Masathai/src/main/java/com/example/masathai/DBUtils/DbConnection.java
@@ -0,0 +1,29 @@
+package com.example.masathai.DBUtils;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+
+public class DbConnection {
+    public Connection dbLink;
+
+    public Connection getConnection(){
+        String dbName = "civikquiz";
+        String dbUser = "root";
+        String dbPassword = "root";
+        String url = "jdbc:mysql://localhost/" + dbName;
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            dbLink = DriverManager.getConnection(url,dbUser,dbPassword);
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return dbLink;
+
+    }
+
+
+}
