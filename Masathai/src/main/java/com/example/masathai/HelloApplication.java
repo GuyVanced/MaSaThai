3c50a056f2de081c317376929b01a2af631daafe
GuyVanced <pandeykushal017@gmail.com>
2023-11-20 10:50:33 +0545

    Login and Register FXML file completed

--- Masathai/src/main/java/com/example/masathai/HelloApplication.java
+++ Masathai/src/main/java/com/example/masathai/HelloApplication.java
@@ -2,6 +2,7 @@ package com.example.masathai;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -10,8 +11,11 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/app/login.fxml"));
+
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root, 320, 240);
+
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
@@ -19,5 +23,6 @@ public class HelloApplication extends Application {
 
     public static void main(String[] args) {
         launch();
+
     }
 }
